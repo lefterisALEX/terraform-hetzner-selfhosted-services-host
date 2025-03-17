57f3fe9915954771db6d9e43c679d85512fc270d
lefteris alexakis <lefteris.alexakis@kpn.com>
2025-03-17 05:18:27 +0100

    feat: use deployr setup script

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -42,27 +42,27 @@ module "server" {
   source = "../.."
 
   name                     = "cloudstack-dev-2"
-  image                    = "ubuntu-22.04"
+  image                    = "ubuntu-24.04"
   server_type              = "cax11"
   region                   = "nbg1"
   volume_size              = 10
-  hcloud_network_id        = 10756354
-  server_ip                = "10.122.0.10"
+  hcloud_network_id        = 10756284
+  server_ip                = "192.168.156.50"
   public_access            = false
   volume_delete_protection = false
   tailscale_auth_key       = var.tailscale_auth_key
-  enable_infisical         = true
   infisical_client_id      = var.infisical_client_id
   infisical_client_secret  = var.infisical_client_secret
   infisical_project_id     = var.infisical_project_id 
   infisical_api_url        = "https://eu.infisical.com"   
   github_repo_url          = "https://github.com/lefterisALEX/terraform-hetzner-cloudstack.git"
   github_token             = var.github_token
+  docker_compose_path      = "examples/basic/apps"      
 
 
   timezone         = "Europe/Amsterdam"
   ssh_keys         = ["main"]
-  tailscale_routes = "10.122.0.10/32,172.29.0.0/16"
+  tailscale_routes = "192.168.156.50/32,172.29.0.0/16"
   custom_userdata = [
      "echo 'Custom user-data execution'",
      "mkdir /mnt/custom_data",
